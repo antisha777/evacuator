0c7ac4a7c30d6546fc4f1c19340b6f26640f1bbb
antisha777 <antisha6666@gmail.com>
2025-10-16 15:58:18 +0700

    feat: add complete landing page for evacuator service
    - Add HTML structure with BEM methodology
    - Implement SCSS with variables, mixins and components
    - Add responsive design
    - Include Montserrat font from Google Fonts
    - Setup Gulp build system

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+
+// Пути к файлам
+const paths = {
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dest: 'dist/css/'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'dist/js/'
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'dist/' // БЫЛО: paths.dest - ЭТО ОШИБКА!
+  },
+  images: {
+    src: 'src/images/**/*',
+    dest: 'dist/images/'
+  }
+};
+
+// Компиляция SCSS в CSS
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+// Обработка JavaScript
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browserSync.stream());
+}
+
+// Копирование HTML
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest)) // ИСПРАВЛЕНО: paths.html.dest
+    .pipe(browserSync.stream());
+}
+
+// Копирование изображений (без сжатия)
+function images() {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream());
+}
+
+// Сервер и отслеживание файлов
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    port: 3000,
+    open: true
+  });
+
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.images.src, images);
+}
+
+// Отдельные задачи
+exports.styles = styles;
+exports.scripts = scripts;
+exports.html = html;
+exports.images = images;
+exports.serve = serve;
+
+// Сборка проекта
+const build = gulp.parallel(styles, scripts, html, images);
+
+// Задача по умолчанию
+exports.default = gulp.series(build, serve);
